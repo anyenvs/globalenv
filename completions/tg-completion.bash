159e4f6bf5a05ec2258c2f2de6c49e763de30158
Vlad M <vlad.malinovskyy@ibm.com>
2022-11-30 16:12:28 -0500

    Adding terragrunt debug flags

--- completions/tg-completion.bash
+++ completions/tg-completion.bash
@@ -1,1 +1,7 @@
-complete -C $(which terraform) terragrunt
+ANYENV_ROOT=$HOME/.anyenv
+TFENV_ROOT=$HOME/.tfenv
+GLOBALENV_ROOT=$HOME/.globalenv
+ANYENV_PATHS="${ANYENV_ROOT}/bin:${GLOBALENV_ROOT}/bin:${GLOBALENV_ROOT}/shims:${TFENV_ROOT}/bin:${TFENV_ROOT}/shims"
+PATH=$(echo -n "$HOME/bin:$HOME/.local/bin:${ANYENV_PATHS}:$PATH" | awk -v RS=: -v ORS=: '!x[$0]++' | sed "s/\(.*\).\{1\}/\1/")
+test -z "$( which terraform )" || complete -C $(which terraform) terragrunt
+
