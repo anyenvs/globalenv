e89ca908febacbbafb029f50e6181183c186c459
Vlad_M <vyacheslav.malinovskyy@ibm.com>
2023-03-29 19:51:59 -0400

    KINDENV module

---
+++ completions/kind-completion.bash
@@ -0,0 +1,6 @@
+## Kind completion
+unset POSIXLY_CORRECT
+eval which kind && . <( kind completion ${SHELL##*/} ) || true
+eval which kind && grep -q 'kind completion' $HOME/.bash_completion || echo 'eval which kind && . <( kind completion ${SHELL##*/} )' >> $HOME/.bash_completion
+
+true
