1f98c4db1fe2166841277e36160dc158efbc0529
Vlad_M <vyacheslav.malinovskyy@ibm.com>
2025-03-14 18:20:12 -0400

    Improve to Terragrunt Module

--- completions/canary-checker-completion.bash
+++ completions/canary-checker-completion.bash
@@ -1,5 +1,5 @@
 ## canary-checker Cli completion
-eval which canary-checker && . <( canary-checker completion ${SHELL##*/} ) || true
+eval which canary-checker && . <( canary-checker completion ${SHELL##*/} --log-to-stderr 2>/dev/null ) || true
 grep -q 'canary-checker completion' $HOME/.bash_completion || echo 'eval which canary-checker && . <( canary-checker completion ${SHELL##*/} )' >> $HOME/.bash_completion
 
 true
--- completions/tg-completion.bash
+++ completions/tg-completion.bash
@@ -4,26 +4,28 @@ GLOBALENV_ROOT=$HOME/.globalenv
 ANYENV_PATHS="${ANYENV_ROOT}/bin:${GLOBALENV_ROOT}/bin:${GLOBALENV_ROOT}/shims:${TFENV_ROOT}/bin:${TFENV_ROOT}/shims"
 #PATH=$(echo -n "$HOME/bin:$HOME/.local/bin:${ANYENV_PATHS}:$PATH" | awk -v RS=: -v ORS=: '!x[$0]++' | sed "s/\(.*\).\{1\}/\1/")
 
+SED=( $( which {gsed,sed} 2>/dev/null ) )
 ## Terragrunt Completions
 TG_COMMANDS_ALL="$( terragrunt --help )"
-TG_COMMANDS=$( terragrunt | sed -n '/COMMANDS/,/^$/p' | grep -vE '\*|COMMANDS' | awk '{ print $1 }' )
-TG_OPTIONS=$( terragrunt | sed -n '/GLOBAL OPTIONS/,/^$/p' | grep -vE 'GLOBAL OPTIONS' | awk '{ print $1 }' | xargs -I{} echo {} )
+TG_COMMANDS=$( terragrunt | $SED -n '/\(COMMAND\|commands\|shortcuts\)/,/^$/p' | grep -vE '\*|COMMANDS|commands:|shortcuts:' | awk '{ print $1 }' )
+TG_OPTIONS=$( terragrunt | $SED -n '/\(GLOBAL OPTIONS|Global Options\)/,/^$/p' | grep -vE 'GLOBAL OPTIONS|Global Options' | awk '{ print $1 }' | xargs -I{} echo {} )
+TG_OPTIONS_RUN_ALL=$( terragrunt run --help | $SED -n '/\(GLOBAL OPTIONS\|Options\)/,/^$/p' | grep -vE 'GLOBAL OPTIONS|Global Options|Options:' | awk '{ print $1 }' | xargs -I{} echo {} )
 
 ## Terraform Completions
 TF_COMMANDS_ALL="$( terraform --help )"
-TF_MAIN_COMMANDS=$(  echo "$TF_COMMANDS_ALL" | sed -n '/Main commands/,/^$/p' | grep -vE 'Main commands' | awk '{ print $1 }' )
-TF_GLOBAL_OPTIONS=$( echo "$TF_COMMANDS_ALL" | sed -n '/Global options/,/^$/p' | grep -vE 'Global options' | awk '{ print $1 }' )
-TF_OTHER_COMMANDS=$( echo "$TF_COMMANDS_ALL" | sed -n '/All other commands/,/^$/p' | grep -vE 'All other commands' | awk '{ print $1 }' )
-TF_COMMANDS_APPLY=$( terraform apply -help | sed -n 'H;/Options/h; ${g;p;}' | grep '^  -' | awk '{ print $1 }')
-TF_COMMANDS_INIT=$(  terraform init -help  | sed -n 'H;/Options/h; ${g;p;}' | grep '^  -' | awk '{ print $1 }')
-TF_COMMANDS_TAINT=$( terraform taint -help | sed -n 'H;/Options/h; ${g;p;}' | grep '^  -' | grep -v ',' | awk '{ print $1 }')
-TF_COMMANDS_STATE=$( terraform state -help | sed -n '/Subcommands/,/^$/p' | grep -vE 'Subcommands' | awk '{ print $1 }')
+TF_MAIN_COMMANDS=$(  echo "$TF_COMMANDS_ALL" | $SED -n '/Main commands/,/^$/p' | grep -vE 'Main commands' | awk '{ print $1 }' )
+TF_GLOBAL_OPTIONS=$( echo "$TF_COMMANDS_ALL" | $SED -n '/Global options/,/^$/p' | grep -vE 'Global options' | awk '{ print $1 }' )
+TF_OTHER_COMMANDS=$( echo "$TF_COMMANDS_ALL" | $SED -n '/All other commands/,/^$/p' | grep -vE 'All other commands' | awk '{ print $1 }' )
+TF_COMMANDS_APPLY=$( terraform apply -help | $SED -n 'H;/Options/h; ${g;p;}' | grep '^  -' | awk '{ print $1 }')
+TF_COMMANDS_INIT=$(  terraform init -help  | $SED -n 'H;/Options/h; ${g;p;}' | grep '^  -' | awk '{ print $1 }')
+TF_COMMANDS_TAINT=$( terraform taint -help | $SED -n 'H;/Options/h; ${g;p;}' | grep '^  -' | grep -v ',' | awk '{ print $1 }')
+TF_COMMANDS_STATE=$( terraform state -help | $SED -n '/Subcommands/,/^$/p' | grep -vE 'Subcommands' | awk '{ print $1 }')
 TF_OPTIONS_2="-target -replace -chdir="
 
 
 test -z "$( which tofu )" || complete -W "$( echo $TF_MAIN_COMMANDS $TF_OTHER_COMMANDS $TF_GLOBAL_OPTIONS $TF_COMMANDS_APPLY $TF_COMMANDS_INIT $TF_COMMANDS_TAINT $TF_COMMANDS_STATE $TF_OPTIONS_2 | xargs)" tofu
-test -z "$( which terragrunt )" || complete -W "$( echo $TG_COMMANDS $TG_OPTIONS $TF_MAIN_COMMANDS $TF_OTHER_COMMANDS $TF_GLOBAL_OPTIONS $TF_COMMANDS_APPLY $TF_COMMANDS_INIT $TF_COMMANDS_TAINT $TF_COMMANDS_STATE $TF_OPTIONS_2 | xargs)" terragrunt tg tg-debug tg-run-all-debug
-test -z "$( which terragrunt )" || complete -W "$( echo $TG_COMMANDS $TG_OPTIONS $TF_MAIN_COMMANDS $TF_OTHER_COMMANDS $TF_GLOBAL_OPTIONS $TF_COMMANDS_APPLY $TF_COMMANDS_INIT $TF_COMMANDS_TAINT $TF_COMMANDS_STATE $TF_OPTIONS_2 -pattern= -modules-only -plan +plan -verbose dry-run | xargs)" tgp
+test -z "$( which terragrunt )" || complete -W "$( echo $TG_COMMANDS $TG_OPTIONS $TG_OPTIONS_RUN_ALL $TF_MAIN_COMMANDS $TF_OTHER_COMMANDS $TF_GLOBAL_OPTIONS $TF_COMMANDS_APPLY $TF_COMMANDS_INIT $TF_COMMANDS_TAINT $TF_COMMANDS_STATE $TF_OPTIONS_2 | xargs)" terragrunt tg tg-debug tg-run-all-debug tg-source-update-false
+test -z "$( which terragrunt )" || complete -W "$( echo $TG_COMMANDS $TG_OPTIONS $TG_OPTIONS_RUN_ALL $TF_MAIN_COMMANDS $TF_OTHER_COMMANDS $TF_GLOBAL_OPTIONS $TF_COMMANDS_APPLY $TF_COMMANDS_INIT $TF_COMMANDS_TAINT $TF_COMMANDS_STATE $TF_OPTIONS_2 -pattern= -modules-only -plan +plan -verbose dry-run | xargs)" tgp
 test -z "$( which terraform )"  || { complete -C $(which terraform) terraform t ; echo complete -W "$TF_OPTIONS_2" terraform t ; }
 set +x
 
